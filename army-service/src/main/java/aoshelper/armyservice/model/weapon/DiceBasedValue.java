84d28255f8204fb489d830d18a12c8bf02772e9a
Marcin Lusa <mlooser@gmail.com>
2024-10-29 09:55:59 +0100

    Attack and Damage unit tests

--- army-service/src/main/java/aoshelper/armyservice/model/weapon/DiceBasedValue.java
+++ army-service/src/main/java/aoshelper/armyservice/model/weapon/DiceBasedValue.java
@@ -2,29 +2,33 @@ package aoshelper.armyservice.model.weapon;
 
 public interface DiceBasedValue {
     default boolean isDiceBased() {
-        return getValue().contains("D");
+        return getUpperCaseValue().contains("D");
     }
 
     default int getDiceCount() {
         if (isDiceBased()) {
-            return Integer.parseInt(getValue().split("D")[0]);
+            return Integer.parseInt(getUpperCaseValue().split("D")[0]);
         }
         return 0;
     }
 
     default int getDiceSides() {
         if (isDiceBased()) {
-            return Integer.parseInt(getValue().split("D")[1]);
+            return Integer.parseInt(getUpperCaseValue().split("D")[1]);
         }
         return 0;
     }
 
     default int getFixedValue() {
         if (!isDiceBased()) {
-            return Integer.parseInt(getValue());
+            return Integer.parseInt(getUpperCaseValue());
         }
         return 0;
     }
 
     String getValue();
+
+    private String getUpperCaseValue() {
+        return getValue().toUpperCase();
+    }
 }
